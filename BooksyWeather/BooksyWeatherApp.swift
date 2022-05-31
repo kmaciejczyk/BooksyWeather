8fca73cb4ca29370cafbad40a47b2afa3d8a33cb
Paweł Madej <pawel.madej@amukana.pl>
2022-05-31 16:47:08 +0200

    Initial Commit

---
+++ BooksyWeather/BooksyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksyWeatherApp.swift
+//  BooksyWeather
+//
+//  Created by Paweł Madej on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
