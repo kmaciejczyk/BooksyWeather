ab7a64a3a44c1c484378002b2ec9739f23c99478
Paweł Madej <pawel.madej@amukana.pl>
2022-06-02 20:00:52 +0200

    improve welcome screen

--- BooksyWeather/Presentation/MainNavigationView.swift
+++ BooksyWeather/Presentation/MainNavigationView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct MainNavigationView: View {
-    @State private var selectedTab = Tab.forecast
+    @State private var selectedTab = Tab.currentWeather
     @State private var exportedColor = Color.orange
-    @State private var showWelcomeView = false
+    @State private var showWelcomeView: Bool
+
+    init() {
+        print(UserDefaultsConfig.showWelcomeView)
+        _showWelcomeView = State(initialValue: UserDefaultsConfig.showWelcomeView)
+    }
+
     var body: some View {
         TabView(selection: $selectedTab) {
             CurrentWeatherView(tab: $selectedTab, exportedColor: $exportedColor)
@@ -18,16 +24,19 @@ struct MainNavigationView: View {
                     Label("Current", systemImage: "sun.max.fill")
                 }
                 .tag(Tab.currentWeather)
+
             AirPolutionView(tab: $selectedTab, exportedColor: $exportedColor)
                 .tabItem {
-                    Label("Air Polution", systemImage: "aqi.low")
+                    Label("Air Polution", systemImage: "aqi.medium")
                 }
                 .tag(Tab.currentWeather)
+
             ForecastView(tab: $selectedTab, exportedColor: $exportedColor)
                 .tabItem {
                     Label("Forecast", systemImage: "calendar")
                 }
                 .tag(Tab.forecast)
+
             SearchView(tab: $selectedTab, exportedColor: $exportedColor)
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
@@ -36,7 +45,6 @@ struct MainNavigationView: View {
         }
         .fullScreenCover(isPresented: $showWelcomeView) {
             WelcomeView()
-//                .ignoresSafeArea()
         }
     }
 }
@@ -45,8 +53,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainNavigationView()
     }
-}
-
-enum Tab {
-    case currentWeather, forecast, cityChoice, airPolution
 }
--- BooksyWeather/Presentation/WelcomeView.swift
+++ BooksyWeather/Presentation/WelcomeView.swift
@@ -10,34 +10,49 @@ import SwiftUI
 struct WelcomeView: View {
     @Environment(\.presentationMode) var presentationMode
 
+    @State var showWelcomeView: Bool
+
+    init() {
+        _showWelcomeView = State(initialValue: UserDefaultsConfig.showWelcomeView)
+    }
+
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Color(red: 41/255, green: 44/255, blue: 51/255).ignoresSafeArea()
 
-        VStack {
-            Text("Welcome to Booksy Weather")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
+            Toggle("Show welcome again?", isOn: $showWelcomeView)
+                .tint(.purple)
                 .foregroundColor(.white)
-            Image("logo")
-                .resizable()
-                .scaledToFit()
-                .padding(50)
-            Text("1st choice weather app")
-                .foregroundColor(.white)
-                .font(.title)
-            Spacer()
-            Text("Tap anywhere to continue")
-                .font(.subheadline)
-                .foregroundColor(.white)
+                .padding([.bottom, .horizontal])
+                .zIndex(10)
+
+            VStack {
+                Text("Welcome to Booksy Weather")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(40)
+                Text("1st choice weather app")
+                    .foregroundColor(.white)
+                    .font(.title)
+
+                Spacer()
+
+                Text("Tap anywhere to continue")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+            }
+            .contentShape(Rectangle())
+            .padding(.top, 60)
+            .onTapGesture {
+                presentationMode.wrappedValue.dismiss()
+            }
         }
-        .onTapGesture {
-            presentationMode.wrappedValue.dismiss()
-        }
-        .padding(.top, 40)
-        }
-        .onTapGesture {
-            presentationMode.wrappedValue.dismiss()
+        .onChange(of: showWelcomeView) { value in
+            UserDefaultsConfig.showWelcomeView = value
         }
     }
 }
